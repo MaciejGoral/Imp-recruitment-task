6d44e3e2ed25c83cff426924492fea37a1ec6c37
Maciej Goral <maciej636107@gmail.com>
2022-06-07 12:05:10 +0200

    Fixed tests

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import sys
 def test_all():
     tests=['1','2','3','4','5','6','7','8a','8b','9a','9b','10a','10b']
     for t in tests:
-        stream = os.popen('python slonie.py test_files\\slo'+t+'.in')
+        stream = os.popen('python slonie.py < test_files\\slo'+t+'.in')
         f = open("test_files\\slo"+t+".out", "r")
         n = int(f.readline())
         output=int(stream.read())
